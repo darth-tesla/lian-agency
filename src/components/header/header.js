f74cdf347d80e7427ca3b1f605f5d5acf01fd005
darth-tesla <darth_tesla@protonmail.ch>
2020-03-12 17:24:57 +0300

    make responsive header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,17 @@
 import * as $ from "jquery";
 
 $(document).ready(function() {
-  $(".hamburger").on("click", function() {
-    $(this).toggleClass("is-active");
+  $(".hamburger, .header__fog-area, .header__link").on("click", function() {
+    $(".hamburger").toggleClass("is-active");
     $(".header__nav").slideToggle();
+  });
+
+  $(window).resize(function() {
+    if ($(window).width() > 768) {
+      $(".header__nav").removeAttr("style");
+      if ($(".hamburger").hasClass("is-active")) {
+        $(".hamburger").removeClass("is-active");
+      }
+    }
   });
 });
