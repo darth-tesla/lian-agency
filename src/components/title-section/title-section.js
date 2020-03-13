63dd79799e80a5bb191610cc988efdb37f64c460
darth-tesla <darth_tesla@protonmail.ch>
2020-03-13 18:22:38 +0300

    make header slider (w/o dark fog and responsive)

--- src/components/title-section/title-section.js
+++ src/components/title-section/title-section.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $(".title-section__slider").slick({
     infinite: true,
-    slidesToShow: 1
+    slidesToShow: 1,
+    dots: true,
+    prevArrow: "<div class='title-section__prev-arrow'><i class='fas fa-chevron-left'></i></div>",
+    nextArrow: "<div class='title-section__next-arrow'><i class='fas fa-chevron-right'></i></div>"
   });
 });
